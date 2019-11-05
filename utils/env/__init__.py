82f5e7df8e3869632cf6169dee8e67091c0388ae
Ashish Gaurav <agaurav77@yahoo.com>
2019-11-05 07:00:35 -0500

    add ppo eval functions and misc utils.env functions

--- utils/env/__init__.py
+++ utils/env/__init__.py
@@ -1,2 +1,3 @@
-from .common import episode
+from .common import episode, n_episodes, evaluate_ppo, \
+    get_ob_rms, wrap_env, vectorize_env
 from .gym_wrappers import NormalizedActions, TimeLimit, VecNormalize, VecPyTorch
