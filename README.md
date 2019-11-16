c03e664d8a0ce46fd1d6d564e97363a6ef14ccb5
Ashish Gaurav <agaurav77@yahoo.com>
2019-11-16 03:01:08 -0500

    Integrate with wm2 and add learning envs (#2)
    * add submodule wm2
    
    * Change wm2 IntersectionOnlyEgoEnv reward structure
    
    * downgrade openai baselines requirement
    
    * PPO: minor logging format change
    
    * PPO on intersection ego from left to right, 30k
    
    * Only extra feature is stopped, trains in 120k steps
    
    * PPO: log how many episodes are done
    
    * PolicyPPO: save/load model with ob_rms
    
    * add ppo eval functions and misc utils.env functions
    
    * Fix reward structure in submodule wm2 for IntersectionOnlyEgoEnv
    
    * ppo_intersection_ego_lr, 35k with evaluation working
    
    * Rename IntersectionOnlyEgoEnv -> NoStoppedCarEnv
    
    * Show reasons for terminations while training
    
    * Add examples/ppo_one_stopped_car.py
    
    * Two stopped cars with clip_param = 0.3, 100k steps
    
    * use clip 0.3 for all envs, document running times, 0, 1, 2 cars working
    
    * add three stopped cars sso env (130k steps)
    
    * 0,1,2,3 stopped car envs all have 13 features (zero padded); retrained
    
    * bump to use wm2 recent master

--- README.md
+++ README.md
@@ -6,4 +6,11 @@ RL experiments in Pytorch for catastrophic forgetting.
 * PPO ([ikostrikov/pytorch-a2c-ppo-acktr-gail](https://github.com/ikostrikov/pytorch-a2c-ppo-acktr-gail))
 
 **Requirements**: Install using `pip3 install -r requirements.txt --user`<br>
-**Examples**: Run an example as `python examples/EXAMPLE.py`
+**Examples**: Run an example as `python examples/EXAMPLE.py`; note that some examples need `xvfb-run` <br>
+**Models**: Saved models in `models`
+
+**Environments (8 processes, clip = 0.3)**:
+* `NoStoppedCarEnv`: `27648 steps, 525 episodes, 0:00:22`
+* `OneStoppedCarEnv`: `20480 steps, 473 episodes, 0:00:18`
+* `TwoStoppedCarsEnv`: `100352 steps, 1942 episodes, 0:01:57`
+* `ThreeStoppedCarsSSOEnv`: `130048 steps, 2437 episodes, 0:02:00`
--- utils/env/common.py
+++ utils/env/common.py
@@ -1,6 +1,11 @@
 from inspect import isfunction
 import numpy as np
 import sys
+from .gym_wrappers import *
+from baselines import bench
+import utils.torch
+from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
+from baselines.common.vec_env.shmem_vec_env import ShmemVecEnv
 
 def episode(env, policy, process_experience, train,
     episode_num = None, render = False, debug = True, tb = None,
@@ -22,20 +27,144 @@ def episode(env, policy, process_experience, train,
         for _ in range(n_updates): losses += [train()]
         T += 1
         R += reward
+        obs = next_obs
         if render: env.render()
-    losses = np.mean(losses, axis = 0)
-    losses = np.round(losses, 6)
+    try:
+        losses = np.mean(losses, axis = 0)
+        losses = np.round(losses, 6)
+    except:
+        losses = -np.inf
 
     if debug:
         extra_str = ""
         if history is not None:
             history += [R]
             extra_str = ", R%d:%.2f" % (smooth, np.mean(history[-smooth:]))
-        print("Ep:%d, T:%d, R:%.2f%s, Loss:%s" % (episode_num, T, R, extra_str, losses))
-    
+        if losses != -np.inf:
+            print("Ep:%d, T:%d, R:%.2f%s, Loss:%s" % (episode_num, T, R, extra_str, losses))
+        else:
+            print("Ep:%d, T:%d, R:%.2f%s" % (episode_num, T, R, extra_str))
+
     if tb != None:
         tb.log([R])
 
     return T, R
 
+def n_episodes(env, policy, n_episodes = 10,
+    render = False, debug = True):
 
+    assert(env != None)
+    assert(isfunction(policy))
+    print("Evaluating for %d episodes." % n_episodes)
+
+    train = lambda: None
+    process_experience = lambda obs, action, reward, next_obs, done: None
+
+    for ep in range(n_episodes):
+
+        episode(env, policy, process_experience, train,
+            episode_num = ep+1, render = render, debug = True)
+
+def get_ob_rms(envs):
+    if isinstance(envs, VecNormalize):
+        return envs.ob_rms
+    elif hasattr(envs, 'venv'):
+        return get_ob_rms(envs.venv)
+    return None
+
+def evaluate_ppo(network, ob_rms, env, device, num_episodes = 10,
+    time_limit = 400, render = False):
+
+    orig_env = env
+    env = wrap_env(
+        env,
+        action_normalize = True,
+        time_limit = time_limit,
+        deterministic = True,
+    )
+    env_fn = lambda: env
+    envs = vectorize_env(
+        [env_fn],
+        state_normalize = True,
+        device = device,
+        train = False,
+        ob_rms = ob_rms
+    )
+    
+    eval_episode_rewards = []
+    orig_env.seed(len(eval_episode_rewards)+1000)
+    obs = envs.reset()
+    while len(eval_episode_rewards) < num_episodes:
+        with torch.no_grad():
+            _, action, _ = network.act(
+                obs,
+                deterministic=True)
+
+        # Obser reward and next obs
+        obs, _, done, infos = envs.step(action)
+        if render: orig_env.render()
+
+        for info in infos:
+            if 'episode' in info.keys():
+                eval_episode_rewards.append(info['episode']['r'])
+                extra = ""
+                if "satisfactions" in info:
+                    extra = ", Reasons: %s" % info["satisfactions"]
+                print("R:%.2f, T:%d%s" % (info['episode']['r'], info['episode']['l'], extra))
+                orig_env.seed(len(eval_episode_rewards)+1000)
+                obs = envs.reset() # Only works for one env (TODO)
+
+    # envs.close()
+
+    print("Evaluation using {} episodes: mean reward {:.5f}\n".format(
+        len(eval_episode_rewards), np.mean(eval_episode_rewards)))
+
+
+# Wrap single env
+def wrap_env(
+    env,
+    action_normalize = True,
+    time_limit = None,
+    deterministic = True,
+    seed = 0):
+
+    if action_normalize:
+        env = NormalizedActions(env)
+    if time_limit != None:
+        env = TimeLimit(env, time_limit)
+    if deterministic:
+        env = utils.torch.make_deterministic(env)
+    env.seed(seed)
+    env = bench.Monitor(env, filename = None, allow_early_resets = True)
+    return env
+
+# Vectorize envs
+def vectorize_env(
+    envs,
+    state_normalize = True,
+    device = None,
+    train = True,
+    gamma = 0.99,
+    ob_rms = None):
+
+    assert(type(envs) == list)
+    for env_fn in envs: assert(isfunction(env_fn))
+    
+    if len(envs) == 1:
+        envs = DummyVecEnv(envs)
+    else:
+        envs = ShmemVecEnv(envs, context = 'fork')
+
+    if state_normalize:
+        if train:
+            envs = VecNormalize(envs, gamma = gamma)
+        else:
+            envs = VecNormalize(envs, ret = False)
+            envs.eval()
+            assert(ob_rms != None)
+            envs.ob_rms = ob_rms
+    
+    if device != None:
+        envs = VecPyTorch(envs, device)
+    
+    return envs
