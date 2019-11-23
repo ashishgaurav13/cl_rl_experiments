eab2748e1fed3fa8dae69c3ce137ecdf305537af
Ashish Gaurav <agaurav77@yahoo.com>
2019-11-22 23:33:53 -0500

    Investigate effect of network size on learning time with no learning schedule for all 5 envs

--- README.md
+++ README.md
@@ -5,13 +5,18 @@ RL experiments in Pytorch for catastrophic forgetting.
 * Twin Delayed DDPG ([sfujim/TD3](https://github.com/sfujim/TD3))
 * PPO ([ikostrikov/pytorch-a2c-ppo-acktr-gail](https://github.com/ikostrikov/pytorch-a2c-ppo-acktr-gail))
 
-**Requirements**: Install using `pip3 install -r requirements.txt --user`<br>
-**Examples**: Run an example as `python examples/EXAMPLE.py`; note that some examples need `xvfb-run` <br>
+**Requirements**: Install using `pip3 install -r requirements.txt --user`
+
 **Models**: Saved models in `models`
 
-**Environments (8 processes, clip = 0.3)**:
+**Statistics for Environments with PPO (8 processes, clip = 0.3)**:
 * `NoStoppedCarEnv`: `27648 steps, 525 episodes, 0:00:22`
 * `OneStoppedCarOEnv`: `21504 steps, 366 episodes, 0:00:21`
 * `OneStoppedCarEnv`: `20480 steps, 473 episodes, 0:00:18`
 * `TwoStoppedCarsEnv`: `100352 steps, 1942 episodes, 0:01:57`
 * `ThreeStoppedCarsSSOEnv`: `130048 steps, 2437 episodes, 0:02:00`
+
+To reproduce, run `python examples/ppo_ENV_NAME.py`. If you have an x-server error, re-run through `xvfb-run`.
+
+**Experiments**:
+* Effect of network size vs number of steps needed to solve, for various environments
---
+++ experiments/effect_of_network_size.py
@@ -0,0 +1,43 @@
+import utils
+utils.nowarnings()
+
+import sys; sys.path.append('wm2')
+import wm2.craft as craft
+import wm2.tools.misc as utilities
+import utils.torch, utils.env, utils, learn
+import torch
+import json
+import numpy as np
+
+all_envs = {
+    0: ("models/ob_rms/no_stopped_car.pt", craft.NoStoppedCarEnv),
+    0.5: ("models/ob_rms/one_stopped_car_o.pt", craft.OneStoppedCarOEnv),
+    1: ("models/ob_rms/one_stopped_car.pt", craft.OneStoppedCarEnv),
+    2: ("models/ob_rms/two_stopped_cars.pt", craft.TwoStoppedCarsEnv),
+    3: ("models/ob_rms/three_stopped_cars.pt", craft.ThreeStoppedCarsSSO),
+}
+
+steps = 1.5e5 # 1e6
+network_sizes = [32, 50, 64, 100, 128, 150, 200, 250, 256, 300]
+
+steps_to_solve = utils.json_load(f = "graphs/ens.txt", show = True)
+
+for env_id in all_envs.keys():
+    
+    if str(env_id) not in steps_to_solve:
+        steps_to_solve[str(env_id)] = {}
+    
+    for hidden in network_sizes:
+        
+        if str(hidden) in steps_to_solve[str(env_id)]:
+            continue
+        
+        ob_rms_fname, env = all_envs[env_id]
+        [ob_rms] = torch.load(ob_rms_fname)
+        policy, _, total_num_steps = \
+            learn.train_ppo(env, steps, ob_rms = ob_rms, hidden = hidden,
+                linear_schedule = False, clip_param = 0.3)
+        steps_to_solve[str(env_id)][str(hidden)] = total_num_steps
+        utils.json_dump(steps_to_solve, f = "graphs/ens.txt")
+
+utils.json_dump(steps_to_solve, f = "graphs/ens.txt", show = True)
